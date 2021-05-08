2a8d168d173441dc2b662543c33f6a6e4ad1a973
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-08 14:55:51 -0300

    feat: Fetching GoG API data

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,13 +1,15 @@
 'use strict';
-
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
 module.exports = {
-  populate: (ctx) => {
-    console.log("Initialize")
-    ctx.send({ ok: true })
+  populate: async (ctx) => {
+    console.log("Starting to populate...")
+
+    await strapi.services.game.populate()
+
+    ctx.send("Finished populating!")
   }
 };
