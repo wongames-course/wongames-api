85cb36c4752f12e48cc513037c71020891838c8b
Geraldo Luiz <geraldobl58@gmail.com>
2021-05-10 17:54:49 -0300

    feat: Creating data with service create

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -6,6 +6,7 @@
  */
 
 const axios = require("axios");
+const slugify = require("slugify");
 
 async function getGameInfo(slug) {
   const jsdom = require("jsdom");
@@ -29,6 +30,17 @@ module.exports = {
     const { data: { products } } = await axios.get(gogApiUrl);
 
     console.log(products[0]);
-    console.log(await getGameInfo(products[0].slug));
+
+    await strapi.services.publisher.create({
+      name: products[0].publisher,
+      slug: slugify(products[0].publisher).toLowerCase()
+    });
+
+    await strapi.services.developer.create({
+      name: products[0].developer,
+      slug: slugify(products[0].developer).toLowerCase()
+    });
+
+    // console.log(await getGameInfo(products[0].slug));
   }
 };
