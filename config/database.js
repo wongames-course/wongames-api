58a50641d568befdbb0d47f40d14d59f3c577966
Geraldo Luiz <geraldobl58@gmail.com>
2021-08-27 11:11:44 -0300

    :sparkles: Creating Single Type for Recommended Games

--- config/database.js
+++ config/database.js
@@ -9,7 +9,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'wongames'),
         username: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', '123456'),
+        password: env('DATABASE_PASSWORD', 'postgres'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
