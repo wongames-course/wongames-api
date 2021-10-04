4d127500d9384dad1e15763019ec1db0af351cac
Geraldo Luiz <geraldobl58@gmail.com>
2021-10-04 14:22:18 -0300

    :sparkles: Customizing controllers in Strapi to search by user authenticated

--- api/wishlist/controllers/wishlist.js
+++ api/wishlist/controllers/wishlist.js
@@ -1,8 +1,23 @@
 'use strict';
 
+const { sanitizeEntity } = require("strapi-utils")
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    const token  = await strapi.plugins["user-permissions"].services.jwt.getToken(ctx);
+
+    const body = {
+      ...ctx.request.body,
+      user: token.id
+    }
+
+    const entity = await strapi.services.wishlist.create(body);
+
+    return sanitizeEntity(entity, { model: strapi.models.wishlist });
+  }
+};
